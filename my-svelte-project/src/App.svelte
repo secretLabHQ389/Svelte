4dca2f1e6edd118fccb8e4fca15e3b33bed50222
GeorgeP225 <gpayne@greeningaustralia.org.au>
2021-12-04 22:51:43 +1100

    Reactivity section

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,9 +1,34 @@
-
-
 <script lang="ts">
 	import Nested from './Nested.svelte'
 	export let name;
 	export let unescapedText;
+
+	let count: number = 0;
+	$: doubled = count * 2; //'reactive values- changes when above value does'
+	$: {
+		console.log(`the count is ${count}`);
+		alert(`Reactive alert for the count ${count}.`);
+		if (count >= 5) {
+			alert('Count is too high.')
+		}
+	}
+
+	const incrementCounter = () => {
+		count += 1
+	}
+
+	//array methods, like .push() don't give responding data because doesn't change assignment
+	//rather, assign a new variable to show a change:
+	let guestList: string[] = ['guest'];
+	//you need to redefine varaibles by assignment, meaning it requires an = sign
+	const addAnother = () => {
+		guestList = [...guestList, 'guest']
+	}
+	//when redefining a property, needs to assign the new object to the variable name:
+	//const foo = obj.foo;
+	//foo.bar = 'baz';
+	//follow that up with this or it won't update:
+	//obj = obj
 </script>
 
 <main>
@@ -12,6 +37,10 @@
 	<img src={'images/imageExample.png'} alt='{name} used inside alt as variable' />
 	<Nested/>
 	<p>{@html unescapedText}</p>
+	<button on:click={incrementCounter}>Clicked {count} {count === 1 ? 'time' : 'times'}</button>
+	<p>Doubled count: {doubled}</p>
+	<button on:click={addAnother}>Add another invite:</button>
+	<p>{guestList}</p>
 </main>
 
 <style>
