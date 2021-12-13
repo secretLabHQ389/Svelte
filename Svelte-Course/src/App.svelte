9b160d3cbe198bfde7a912db677cd144c3f5f5b1
GeorgeP225 <gpayne@greeningaustralia.org.au>
2021-12-14 01:20:35 +1100

    Basic syntax

--- Svelte-Course/src/App.svelte
+++ Svelte-Course/src/App.svelte
@@ -2,7 +2,7 @@
 	//lect. 16 4:30, calls this 'Pascal Case', as I said:
 	import ContactCard from './ContactCard.svelte'
 
-	export let name, age;
+	export let name, age, job = 'plummer', bio = 'popular with friends, do their plumming';
 
 	$: upperCaseName = name.toUpperCase()
 	$: console.log(name)
@@ -28,6 +28,16 @@
 <h1>Hello {name} age {age}! Hey, {upperCaseName}.</h1>
 <button on:click={incrementAge}>Next Year</button>
 <button on:click={changeName}>Name: Ryan</button>
+Enter Name:
 <input type='text' bind:value={name} />
+Job:
+<input type="text" bind:value={job} />
+Biography
+<textarea type="text" bind:value={bio} />
 
-<ContactCard />
+<ContactCard 
+	userName={name} 
+	jobTitle={job}
+	description={bio}
+	imageProp={'https://d8oklrjckdahn.cloudfront.net/library/MwGo/2017/3/7/11G132324/Images/3_Bears_Approach_by_Jim_Lawrence-4f4d16b51311853286570ff0d3022a81.jpg'}
+	/>
--- Svelte-Course/src/ContactCard.svelte
+++ Svelte-Course/src/ContactCard.svelte
@@ -1,81 +1,90 @@
-<style>
-    .contact-card {
-      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-      max-width: 30rem;
-      border-radius: 5px;
-      margin: 1rem 0;
-    }
-
-    .height100p {
-        height: 100%;
-    }
-
-    .marginpoint5rem {
-        margin: .5rem;
-    }
+<script>
+  export let userName, jobTitle, description, imageProp
+</script>
   
-    header {
-      height: 7rem;
-      padding: .5rem;
-    }
-  
-    .grid-container {
-      display: grid;
-      grid-template-columns: 33% 67%;
-      justify-content: space-between;
-    }
-  
-    .thumb {
-      grid-column-start: 1;
-      grid-column-end: 2;
-    }
-  
-    img {
-      width: 100%;
-      object-fit: cover;
-    }
-  
-    .user-data {
-      grid-column-start: 2;
-      grid-column-end: 2;
-      flex-direction: column;
-      justify-content: center;
-    }
-  
-    h1 {
-      font-size: 1.25rem;
-      font-family: "Roboto Slab", sans-serif;
-      margin: 0.5rem 0;
-    }
-  
-    h2 {
-      font-size: 1rem;
-      font-weight: normal;
-      color: #5a5a5a;
-      margin: 0;
-      margin-bottom: 0.5rem;
-    }
-  
-    .description {
-      border-top: 1px solid #ccc;
-      padding: 1rem;
-    }
-  </style>
-  
-  <div class="contact-card">
-    <header>
-      <div class="grid-container">
-        <div class="thumb height100p marginpoint5rem">
-          <img class="height100p" src="" alt="" />
-        </div>
-        <div class="user-data marginpoint5rem">
-          <h1>User Name</h1>
-          <h2>Job Title</h2>
-        </div>
+<div class="contact-card">
+  <header>
+    <div class="grid-container">
+      <div class="thumb height100p marginpoint5rem">
+        <img class="height100p" src={imageProp} alt="Attempt at prop, three happy bears walking near brook." />
       </div>
-    </header>
-    <div class="description">
-      <p>A short description</p>
+      <div class="user-data marginpoint5rem">
+        <h1>{userName}</h1>
+        <!-- override text escaping: -->
+        <h2>Job: {@html jobTitle}</h2>
+      </div>
     </div>
+  </header>
+  <!-- <div class={description ? "description" : "description no-desc"}> -->
+  <div class="description" class:no-desc="{!description}">
+    <p>{description ? description : 'A short description'}</p>
   </div>
+</div>
   
+<style>
+  .contact-card {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    max-width: 30rem;
+    border-radius: 5px;
+    margin: 1rem 0;
+  }
+
+  .height100p {
+      height: 100%;
+  }
+
+  .marginpoint5rem {
+      margin: .5rem;
+  }
+
+  header {
+    height: 7rem;
+    padding: .5rem;
+  }
+
+  .grid-container {
+    display: grid;
+    grid-template-columns: 33% 67%;
+    justify-content: space-between;
+  }
+
+  .thumb {
+    grid-column-start: 1;
+    grid-column-end: 2;
+  }
+
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
+
+  .user-data {
+    grid-column-start: 2;
+    grid-column-end: 2;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: 1.25rem;
+    font-family: "Roboto Slab", sans-serif;
+    margin: 0.5rem 0;
+  }
+
+  h2 {
+    font-size: 1rem;
+    font-weight: normal;
+    color: #5a5a5a;
+    margin: 0;
+    margin-bottom: 0.5rem;
+  }
+
+  .description {
+    border-top: 1px solid #ccc;
+    padding: 1rem;
+  }
+
+  .no-desc {
+    background-color: lightblue;
+  }
+</style>
