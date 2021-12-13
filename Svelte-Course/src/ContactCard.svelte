c68e599f2dd66bc49659a9dc95500587211989e9
GeorgeP225 <gpayne@greeningaustralia.org.au>
2021-12-14 00:06:30 +1100

    Setup components

--- Svelte-Course/src/ContactCard.svelte
+++ Svelte-Course/src/ContactCard.svelte
@@ -1,22 +1,81 @@
-<script>
-
-</script>
-
-<div>
-    <header>
-        <div>
-            <img src=" " alt=" " />
-        </div>
-        <div>
-            <h1>User Name</h1>
-            <h2>Job Title</h2>
-        </div>
-    </header>
-    <div>
-        <p>A short description</p>
-    </div>
-</div>
-
 <style>
+    .contact-card {
+      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+      max-width: 30rem;
+      border-radius: 5px;
+      margin: 1rem 0;
+    }
 
-</style>
+    .height100p {
+        height: 100%;
+    }
+
+    .marginpoint5rem {
+        margin: .5rem;
+    }
+  
+    header {
+      height: 7rem;
+      padding: .5rem;
+    }
+  
+    .grid-container {
+      display: grid;
+      grid-template-columns: 33% 67%;
+      justify-content: space-between;
+    }
+  
+    .thumb {
+      grid-column-start: 1;
+      grid-column-end: 2;
+    }
+  
+    img {
+      width: 100%;
+      object-fit: cover;
+    }
+  
+    .user-data {
+      grid-column-start: 2;
+      grid-column-end: 2;
+      flex-direction: column;
+      justify-content: center;
+    }
+  
+    h1 {
+      font-size: 1.25rem;
+      font-family: "Roboto Slab", sans-serif;
+      margin: 0.5rem 0;
+    }
+  
+    h2 {
+      font-size: 1rem;
+      font-weight: normal;
+      color: #5a5a5a;
+      margin: 0;
+      margin-bottom: 0.5rem;
+    }
+  
+    .description {
+      border-top: 1px solid #ccc;
+      padding: 1rem;
+    }
+  </style>
+  
+  <div class="contact-card">
+    <header>
+      <div class="grid-container">
+        <div class="thumb height100p marginpoint5rem">
+          <img class="height100p" src="" alt="" />
+        </div>
+        <div class="user-data marginpoint5rem">
+          <h1>User Name</h1>
+          <h2>Job Title</h2>
+        </div>
+      </div>
+    </header>
+    <div class="description">
+      <p>A short description</p>
+    </div>
+  </div>
+  
